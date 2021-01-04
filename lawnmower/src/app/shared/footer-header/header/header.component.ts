264432eee865a58c60c294f137b3169ad25d722c
Ezazhel <Ezazhel@gmail.com>
2021-01-04 07:53:15 +0100

    create utility folder, file, add toExponential in header

--- lawnmower/src/app/shared/footer-header/header/header.component.ts
+++ lawnmower/src/app/shared/footer-header/header/header.component.ts
@@ -3,31 +3,29 @@ import { select, Store } from '@ngrx/store';
 import { RootStoreState, StatsSelector } from 'app/root-store';
 import { Observable } from 'rxjs';
 @Component({
-  selector: 'app-header',
-  template: `
-    <header>
-      <mat-toolbar color="primary">
-        <div fxHide.gt-xs>
-          <button mat-icon-button (click)="onToggleSideNav()">
-            <mat-icon>menu</mat-icon>
-          </button>
-        </div>
-        <div>Lawnmoner</div>
-        <span> money : {{ money$ | async }}$ </span>
-      </mat-toolbar>
-    </header>
-  `,
-  styleUrls: ['./header.component.scss'],
+    selector: 'app-header',
+    template: `
+        <header>
+            <mat-toolbar color="primary">
+                <div fxHide.gt-xs>
+                    <button mat-icon-button (click)="onToggleSideNav()">
+                        <mat-icon>menu</mat-icon>
+                    </button>
+                </div>
+                <div>Lawnmoner</div>
+                <span> money : {{ (money$ | async).toExponential(2) }}$ </span>
+            </mat-toolbar>
+        </header>
+    `,
+    styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-  @Output() public sidenavToggle = new EventEmitter();
-  public money$: Observable<number> = this.store.pipe(
-    select(StatsSelector.selectStatsMoney)
-  );
-  constructor(private store: Store<RootStoreState.State>) {}
+    @Output() public sidenavToggle = new EventEmitter();
+    public money$: Observable<number> = this.store.pipe(select(StatsSelector.selectStatsMoney));
+    constructor(private store: Store<RootStoreState.State>) {}
 
-  ngOnInit(): void {}
-  onToggleSideNav = () => {
-    this.sidenavToggle.emit();
-  };
+    ngOnInit(): void {}
+    onToggleSideNav = () => {
+        this.sidenavToggle.emit();
+    };
 }
