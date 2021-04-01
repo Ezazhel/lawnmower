c81098876089d0d8dc2030f9089a64f9bebd0832
steven jeanne <steven@jvweb.fr>
2021-04-01 13:47:34 +0200

    Add selected variable in neighboor components

--- lawnmower/src/app/features/money-earning/components/mowing/components/neighboor/neighboor.component.ts
+++ lawnmower/src/app/features/money-earning/components/mowing/components/neighboor/neighboor.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, ChangeDetectionStrategy, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
 import { Neighboor } from '@core/models/neighboor';
 import { Store } from '@ngrx/store';
 import { RootStoreState } from 'app/root-store';
@@ -13,12 +13,14 @@ import { insertOrUpdateNeighboorToCut } from '../../../../../../root-store/neigh
 export class NeighboorComponent implements OnInit {
     @Input()
     neighboor: Neighboor;
-
     constructor(private store: Store<RootStoreState.State>) {}
+
+    wasClicked:  boolean = false;
 
     ngOnInit(): void {}
 
     cut() {
+        this.wasClicked = !this.wasClicked;
         if (this.neighboor.cutPercent == 0) {
             this.store.dispatch(insertOrUpdateNeighboorToCut({ id: this.neighboor.id, cutted: 0 }));
         }
